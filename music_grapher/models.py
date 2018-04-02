d7692001d2e470d5de5f888babf7183356502687
emmetttt <emmettjd@hotmail.co.uk>
2018-04-02 15:30:54 +0100

    models update2

--- music_grapher/models.py
+++ music_grapher/models.py
@@ -74,24 +74,6 @@ class BandSearch:
         pass
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 class PopDB:
     def __init__(self):
         print("PopDB Start")
@@ -158,181 +140,6 @@ class PopDB:
                             album["no_critic_reviews"] = int(rating.findAll("div", {"class": "ratingText"})[-1].text[1:-1])
                         if "user score" in rating.text and "\n" in rating.text:
                             album["user_score"] = int(rating.find("div", {"class": "rating"}).text)
-<<<<<<< HEAD
-                            album["no_user_reviews"] = rating.findAll("div", {"class": "ratingText"})[-1].text
-            if all([item in album for item in ["album_year", "album_link", "album_title"]]):
-                self.albumData.append(album)
-
-    def minMaxSet(self):
-        if len(self.albumData) != 0:
-            albumYears = [x["album_year"] for x in self.albumData]
-            self.max_date = max(albumYears) + 2
-            self.min_date = min(albumYears) - 2 
-
-        if len(self.albumData) != 0:
-            userScores = [x["user_score"] for x in self.albumData]
-            criticScores = [x["critic_score"] for x in self.albumData]
-            scores = userScores + criticScores
-            if min(scores) < 5:
-                self.min_score = min(scores)
-            else:
-                self.min_score = min(scores) - 5
-
-            if max(scores) > 95:
-                self.max_score = max(scores)
-            else:
-                self.max_score = max(scores) + 5
-
-
-
-    def regression(self):
-        if len(self.albumYears) == 0 or len(self.albumScores) == 0:
-            return -1
-
-        avgx = sum(self.albumYears)/len(self.albumYears)
-        avgy = sum(self.albumScores)/len(self.albumScores)
-
-        X = []
-        Y = []
-        sumxy = 0
-        sumx2 = 0
-        sumy2 = 0
-        sumXminusmean = 0
-        sumYminusmean = 0
-
-        for j in range(self.albumYears - 1):
-            X.append(self.albumYears[j] - avgx)
-            Y.append(self.albumScores[j] - avgy)
-            sumxy = sumxy + (Y[i]*X[i])
-            sumx2 = sumx2 + X[i]**2
-            sumy2 = sumy2 + Y[i]*Y[i]
-            sumXminusmean = sumXminusmean + (self.albumYears[i] - avgx)**2
-            sumYminusmean = sumYminusmean + (self.albumScores[i] - avgy)**2
-
-        r = sumxy/math.sqrt(sumx2 * sumy2)
-        stdevx = math.sqrt(sumXminusmean/(len(self.albumYears)-1))
-        stdevy = math.sqrt(sumYminusmean/(len(self.albumScores)-1))
-
-        b = r * (stdevx/stdevy)
-        A = avgy - (b*avgx)
-
-        lineX = [min(self.albumYears), max(self.albumYears)]
-        lineY = [(lineX[0]*b)+A, (lineX[1]*b)+A]
-
-        k=0
-        regression = []
-        for xy in lineX:
-            regression.append([lineX[k],lineY[k]])
-            k+=1
-
-        self.regression = regression
-
-    def jsonify(self):
-        data = []
-        k=0
-        for value in self.albumData: ##Gets data into javascript readable format
-            data.append('{name: "' + value["album_title"] + 
-                        '",x: "' + str(value["album_year"]) + 
-                        '",link: "' + value["album_link"] + 
-                        '",y: ' + str(value["critic_score"]) + '}')
-            data[k] = data[k].replace("'", "")
-            k = k+1
-
-        self.data = str(data).replace("'", "")
-
-    def populateData(self):
-        soup = self.getArtistPage()
-        self.getData(soup)
-        print(self.albumData)
-        self.minMaxSet()
-        #self.regression()
-        self.jsonify()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-class PopDB:
-    def __init__(self):
-        print("PopDB Start")
-        i = self.getLastBand()
-        print("ID: ", i)
-        while i < 3000:#40000:
-            url = 'https://www.albumoftheyear.org/artist/' + str(i) + '/'
-            band_soup = self.openPage(url)
-            self.getBandData(band_soup, i)
-            #Basic data retreived from band page, now get specific album data
-            hrefs = [a.album_link for a in list(Album.objects.filter(band_id=i))]
-            album_id = [int(a.id) for a in list(Album.objects.filter(band_id=i))]
-            count = 0
-            for album_href in hrefs:
-                album_soup = self.openPage(album_href)
-                self.getAlbumData(album_soup, album_id[count])
-                self.getReviewData(album_soup, album_id[count])
-                count+=1
-
-            print("ID: ", i)
-            i+=1
-
-    def openPage(self, page):
-        headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5)'}
-        urllibpage = requests.get(page, headers=headers)
-        soup = BeautifulSoup(urllibpage.text, "html.parser")
-        return soup
-
-    def getLastBand(self):
-        x = Band.objects.latest('band_id')
-        return x.band_id+1
-
-    def getBandData(self, soup, band_fk):
-        bandName = soup.find('h1', {"class": "artistHeadline"}).text
-        albumContainer = soup.find('div', {"class" : "facetContent"})
-        albumList = albumContainer.findChildren()
-        for row in albumList: #for each album found
-            #Is not a subheadline, needs to be split again
-            album = {
-                "band_name": bandName,
-                "album_link": "X",
-                "album_title": "X",
-                "album_type": "X",
-                "critic_score": 0,
-                "no_critic_reviews": 0,
-                "user_score": 0,
-                "no_user_reviews": 0,
-            }
-
-            datarow = row.findChildren()
-            for data in datarow: #for each album find data
-                if data.has_attr("href"):
-                    album["album_link"] = "http://www.albumoftheyear.org" + data.get('href')
-                if data.has_attr("class") and data.get("class")[0] == "albumTitle":
-                    album["album_title"] = data.text
-                #if data.has_attr("class") and data["class"][0] == "type" and "album_type" not in album:
-                if data.has_attr("class") and data.get("class")[0] == "type":
-                    album["album_type"] = data.text
-                if data.has_attr("class") and data.get("class")[0] == 'ratingRowContainer':
-                    ratingdiv = data.findChildren()
-                    for rating in ratingdiv:
-                        if "critic score" in rating.text and "\n" in rating.text:
-                            album["critic_score"] = int(rating.find("div", {"class": "rating"}).text)
-                            album["no_critic_reviews"] = int(rating.findAll("div", {"class": "ratingText"})[-1].text[1:-1])
-                        if "user score" in rating.text and "\n" in rating.text:
-                            album["user_score"] = int(rating.find("div", {"class": "rating"}).text)
-=======
->>>>>>> b2d4da7d36c530951c0017e0a080b8fb36f75d16
                             album["no_user_reviews"] = int(rating.findAll("div", {"class": "ratingText"})[-1].text[1:-1])
             #If data is complete, insert to DB
             if album["critic_score"] + album["user_score"] != 0:
@@ -358,12 +165,8 @@ class PopDB:
             if "Release Date" in row.text:
                 date = row.text
                 date = date[:-15]
-<<<<<<< HEAD
-                album.date = date
-=======
                 album.date = self.getdate(date)
                 print(album.date)
->>>>>>> b2d4da7d36c530951c0017e0a080b8fb36f75d16
             elif "Label" in row.text:
                 label = row.text
                 label = label[:-8]
@@ -372,8 +175,6 @@ class PopDB:
                 genre = row.text
                 genre = genre[:-9]
                 album.genre = genre
-<<<<<<< HEAD
-=======
         #save changes
         album.save()
 
@@ -407,7 +208,6 @@ class PopDB:
             if int(thisday) < 10:
                 thisday = "0" + thisday
         return datetime.date(int(thisyear), int(thismonth), int(thisday))
->>>>>>> b2d4da7d36c530951c0017e0a080b8fb36f75d16
 
     def getReviewData(self, soup, _album_id):
         reviews = soup.find("div", {"id": "critics", "class": "section"})
