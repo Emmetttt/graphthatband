d9d98d1e9d9182979b4860f7774efa90d19b76cb
Emmett <emmettjd@hotmail.co.uk>
2018-04-16 19:38:00 +0000

    merged models

--- music_grapher/models.py
+++ music_grapher/models.py
@@ -1,5 +1,4 @@
 import datetime
-import time
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
@@ -79,7 +78,7 @@ class BandSearch:
         data = []
         k=0
         for album in self.albums: ##Gets data into javascript readable format
-            data.append('{name: "' + album.album_name + 
+            data.append('{name: "' + album.album_name +
                         '",x: "' + self.fract_year(str(album.date)) + '",' +
                         'link: "' + album.album_link + '",' +
                         'y: ' + str(album.critic_score_avg) +
@@ -204,7 +203,6 @@ class PopDB:
                 date = row.text
                 date = date[:-15]
                 album.date = self.getdate(date)
-                print(album.date)
             elif "Label" in row.text:
                 label = row.text
                 label = label[:-8]
@@ -232,20 +230,26 @@ class PopDB:
             "December": "11",
             "November": "12",
         }
-        print(string)
         indiv = string.split(" ")
-        if len(indiv) < 3:
-            thisyear = string.relace(" ")
-            thismonth = 0
-            thisday = 0
+        if len(indiv) == 2:
+            thisyear = indiv[1]
+            thismonth = 1
+            thisday = 1
+        elif len(indiv) == 3:
+            thisyear = indiv[2]
+            thismonth = months[indiv[0]]
+            thisday = 1
         else:
             thisyear = indiv[3]
             thismonth = months[indiv[0]]
             thisday = indiv[1]
             thisday = thisday.replace(",", "")
-            if int(thisday) < 10:
-                thisday = "0" + thisday
-        return datetime.date(int(thisyear), int(thismonth), int(thisday))
+        #print(indiv, int(thisyear), int(thismonth), int(thisday))
+        try:
+            returndate = datetime.date(int(thisyear), int(thismonth), int(thisday))
+        except ValueError:
+            returndate = datetime.date(int(thisyear), int(thismonth), int(thisday)-2)
+        return returndate
 
     def getReviewData(self, soup, _album_id):
         reviews = soup.find("div", {"id": "critics", "class": "section"})
@@ -257,7 +261,8 @@ class PopDB:
                 }
             review["score"] = row.find("span", {"itemprop": "ratingValue"}).text
             review["publication"] = row.find("span", {"itemprop": "name"}).text
-            review["link"] = row.find("a", {"itemprop": "url", "rel": "nofollow"})['href']
+            if row.find("a", {"itemprop": "url", "rel": "nofollow"}):
+                review["link"] = row.find("a", {"itemprop": "url", "rel": "nofollow"})['href']
             Review.objects.create(album_id = _album_id,
                                   score = review["score"],
                                   publication = review["publication"],
